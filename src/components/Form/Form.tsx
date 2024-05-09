91531d7894b7453f8f7dbb47cbb2c0cabb259cf4
Paoo08 <paola.permz@gmail.com>
2024-05-09 08:32:00 -0600

    Conectar Backend con Frontend

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,27 +2,25 @@ import { useEffect, useState } from "react";
 import "./Form.css";
 import Data from "./Data";
 
-const loginData = {
-  email: "vnavarro@ceti.mx",
-  password: "123456",
-};
+const API_URL = "http://localhost:3010/";
+
+// const loginData = {
+//   email: "vnavarro@ceti.mx",
+//   password: "123456",
+// };
 
 function Form() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [showData, setShowData] = useState<boolean>(false);
+  const [user, setUser] = useState<any>(null);
 
-  //   useEffect(() => {
-  //     if (email != loginData.email) {
-  //       alert("Easter Egg!!!");
-  //     }
-  //   }, [email]);
+  useEffect(() => {
+    const userInStorageString = window.localStorage.getItem("user") as string;
+    const userInStorage = JSON.parse(userInStorageString);
+    setUser(userInStorage);
+  }, []);
 
-  //   const handleInputChange = (stateUpdate) => {
-  //     return (event) => {
-  //       stateUpdate(event.target.value);
-  //     };
-  //   };
   const handleOnEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
   };
@@ -31,38 +29,69 @@ function Form() {
   ) => {
     setPassword(event.target.value);
   };
-  //   const handleOnClick = () => {
-  //     if (showData) {
-  //       setEmail("");
-  //       setPassword("");
-  //       setShowData(false);
-  //     }
-  //     //toggle the flag
-  //     else if (email === loginData.email && password === loginData.password) {
-  //       {
-  //         setShowData(true);
-  //       }
-  //     }
-  //     setShowData(!showData);
-  //   };
+
   const handleOnClick = () => {
-    if (showData) {
-      setEmail("");
-      setPassword("");
-      setShowData(false);
-    } else if (email === loginData.email && password === loginData.password) {
-      setShowData(true);
-    } else {
-      setEmail("");
-      setPassword("");
-      setShowData(false);
-      alert("Información incorrecta, vuelva a intentarlo...");
+    // logIn({ email, password });
+    fetchHomework();
+  };
+
+  const logIn = async ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) => {
+    try {
+      const response = await fetch(`${API_URL}api/v1/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.status === 200) {
+        const data = await response.json();
+        setUser(data);
+        window.localStorage.setItem("user", JSON.stringify(data));
+      } else {
+        alert("Usuario o contraseña incorreta...");
+      }
+
+      console.log(Data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchHomework = async () => {
+    try {
+      const response = await fetch(`${API_URL}api/v1/homeworks`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
     }
   };
 
   return (
     <>
-      <Data email={email} password={password} showData={showData} />
+      {user && (
+        <section className="dataContainer">
+          {
+            <>
+              <p>Email: {user.user.email}</p>
+              <p>Nombre: {user.user.name}</p>
+              <p>ID: {user.user.id}</p>
+            </>
+          }
+        </section>
+      )}
       <section className="formContainer">
         <span className="inputContainer">
           <label htmlFor="email">Email:</label>
